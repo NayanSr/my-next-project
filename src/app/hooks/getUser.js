914216215bdbc0cc7084ae34915f3063afcad59b
NayanSuter111 <nayansuter111@gmail.com>
2025-09-16 17:23:25 +0600

    getUser hook comment out

--- src/app/hooks/getUser.js
+++ src/app/hooks/getUser.js
@@ -1,4 +1,4 @@
-'use client'
+/* 'use client'
 import { useSession } from "next-auth/react";
 
 
@@ -6,3 +6,4 @@ export default function getUser() {
   const { data: session, status } = useSession();
   return session
 }
+ */
