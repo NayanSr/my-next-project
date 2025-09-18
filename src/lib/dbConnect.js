a0ff8f3aafa898b53919cbe66f2de8ac4da3f743
NayanSuter111 <nayansuter111@gmail.com>
2025-09-18 18:56:49 +0600

    mongodb

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,17 +1,17 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = process.env.MONGODB_URI;
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 
-export default function dbConnect(collectionName){
-     const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+export default function dbConnect(collectionName) {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
-return client.db(process.env.DB_NAME).collection(collectionName);
+  return client.db(process.env.DB_NAME).collection(collectionName);
 }
 
